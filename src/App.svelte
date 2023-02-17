eb25732fa59cfc888add1fbf4f3b3cee38396b44
MAYOWA <Balogun Mayowa>
2023-02-17 17:36:36 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</svelte:head>
+
+<script>
+  import Header from "./lib/Header.svelte";
+  import Mainpage from "./lib/Mainpage.svelte";
+
+ 
+</script>
+<div>
+  <Header/>
+  <Mainpage />
+</div>
+
+
---
+++ src/lib/Mainpage.svelte
@@ -0,0 +1,185 @@
+<main class="">
+    <div class="lg:pt-24 pt-12 pb-20 px-[5vw] lg:px-[13vw]  lg:bg-[url('https://www.cloudflare.com/static/f66c9352641e2d181c7d3d4f4e80c899/Test_V1.svg')] lg:bg-no-repeat lg:bg-right-top bg-contain">
+        <div class="">
+             <div class="lg:w-[30vw]">
+                <h1 class="text-3xl lg:text-5xl font-semibold">A global network built for the cloud</h1>
+                <div>
+                    <p class="mt-6">Cloudflare is a global network designed to make everything you
+                    connect to the Internet secure, private, fast, and reliable.</p>
+                    <ul class="my-8 benefits">
+                        <li>Secure your websites, APIs, and Internet applications.</li>
+                        <li>Protect corporate networks, employees, and devices.</li>
+                        <li>Write and deploy code that runs on the network edge.</li>
+                    </ul>
+                </div>
+                <div class="flex gap-4 w-fit text-blue-600 font-bold active:border-2 border-blue-600">
+                    <span>
+                        <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="13" cy="13" r="13" fill="#0051c3"></circle>
+                            <path d="M18.5715 13L9.28577 19.5V6.5L18.5715 13Z" fill="white"></path>
+                        </svg>
+                    </span>
+                    <span>What is Cloudfare? (1 min)</span>
+                </div>
+                <div class="font-bold text-sm text-center mt-12 flex flex-col gap-4 lg:flex-row lg:gap-8">
+                    <div class="w-ful text-white  bg-blue-600 py-4 lg:px-6 lg:w-auto" ><a href="#5">Learn More</a></div>
+                    <div class="text-blue-600 py-4 border-2 border-blue-600 lg:w-auto lg:px-6"><a href="#5">Get Started Free</a></div>
+                </div>
+            </div>
+        </div>   
+    </div>
+    <div class="text-white bg-blue-900 px-[5vw] lg:px-[13vw] py-12 font-semibold sm:flex sm:flex-row sm:gap-8 lg:gap-4 lg:items-center">
+        <div class="text-3xl sm:basis-3/5">
+            <h3>Turn economic uncertainty into opportunity. Regain control with Cloudflare.</h3>
+        </div>
+        <div class="flex flex-col gap-4 text-sm font-bold mt-8 sm:basis-1/2 lg:basis-auto sm:mt-0 lg:flex-row lg:items-center lg:justify-center">
+            <div class="text-center py-4 w-full bg-[#f6821f] lg:h-fit lg:w-fit lg:px-6"><span>Talk to an Expert</span></div>
+            <div class="text-center py-4 w-full bg-[#f6821f] lg:h-fit lg:w-fit lg:px-6"><span>Learn More</span></div>
+        </div>
+    </div>
+    <section class="py-10 px-[5vw] lg:px-[13vw] bg-[#f8fbfb]">
+        <h3 class="font-bold text-sm text-[#f63]">WHAT'S NEW</h3>
+        <div class="md:flex md:gap-8">
+            <div class="mt-6 mb-8 border-t border-gray-300 md:flex md:flex-col">
+                <h4 class=" text-3xl font-semibold mt-10 mb-4">
+                    Webinar: How Otrium thwarted a phishing attempt with Cloudflare
+                </h4>
+                <p class="text-sm">
+                    Learn how Otrium secured their almost-entirely-remote workforce against phishing attacks, and greatly reduced their attack surface.
+                </p>
+                <div class="mt-8 md:h-full md:flex md:items-end">
+                    <a class="text-blue-800 font-semibold" href="#5">
+                        <span class="flex items-center"><span>Register</span><span class="text-3xl"><i class='bx bx-chevron-right' ></i></span></span>
+                    </a>
+                </div>
+                
+            </div>
+            <div class="md:mt-6 my-8 border-t border-gray-300 md:flex md:flex-col">
+                <h4 class=" text-3xl font-semibold mt-10 mb-4">
+                    Wall Street Journal: Cloudflare Takes Aim at Inbox Security Threats
+                </h4>
+                <p class="text-sm">
+                    New Cloudflare services help businesses keep phishing, malware, and other cyberattacks out of corporate email accounts.
+                </p>
+                <div class="mt-8 md:h-full md:flex md:items-end">
+                    <a class="text-blue-800 font-semibold" href="#5">
+                        <span class="flex items-center"><span>Read the Article</span><span class="text-3xl"><i class='bx bx-chevron-right' ></i></span></span>
+                    </a>
+                </div>
+            </div>
+            <div class="md:mt-6 my-8 border-t border-gray-300 md:flex md:flex-col">
+                <h4 class=" text-3xl font-semibold mt-10 mb-4">
+                    Why CIOs choose Cloudflare One for their Zero Trust solution
+                </h4>
+                <p class="text-sm">
+                We asked leading CIOs why they switched to Cloudflare One. Here's what they said.
+                </p>
+                <div class="mt-8 md:h-full md:flex md:items-end">
+                    <a class="text-blue-800 font-semibold" href="#5">
+                        <span class="flex items-center"><span>Learn more</span><span class="text-3xl"><i class='bx bx-chevron-right' ></i></span></span>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section class="px-[13vw] mt-16">
+        <div class="font-semibold">
+            <h3 class="text-sm text-[#f63]">EXPLORE OUR PRODUCTS</h3>
+            <h2 class="text-3xl mt-3">The Integrated Global Cloud Network</h2>
+        </div>
+        <div class="py-20">
+            <div class="p-8 border border-gray-300 w-1/3">
+                <div>
+                    <img src="https://www.cloudflare.com/static/01f0e9e70dbb5132df9a1ebc4b978b79/security-fingerprint-privacy-blue.svg" alt=""/>
+                </div>
+                <div class="mb-6 mt-4">
+                    <h3 class="text-lg font-semibold mb-4">Zero Trust Services</h3>
+                    <p class="text-sm">Stop data loss, malware and phishing with the most performant Zero Trust application access and Internet browsing solution.</p>
+                </div>
+                <div>
+                    <ul>
+                        <li><span><span>
+                            <svg width="16" height="13" viewBox="0 0 16 13" fill="#f63" xmlns="http://www.w3.org/2000/svg" class="mr2 orange0">
+                                <path d="M15.472 2.036L13.436 0L5.497 7.94L2.036 4.479L0 6.514L5.497 12.011L15.472 2.036Z" fill="#f63"></path>
+                            </svg>
+                        </span><span>Zero Trust Network Access (ZTNA)</span></span></li>
+                        <li><span><span>
+                            <svg width="16" height="13" viewBox="0 0 16 13" fill="#f63" xmlns="http://www.w3.org/2000/svg" class="mr2 orange0">
+                                <path d="M15.472 2.036L13.436 0L5.497 7.94L2.036 4.479L0 6.514L5.497 12.011L15.472 2.036Z" fill="#f63"></path>
+                            </svg>
+                        </span><span>Secure Web Gateway (SWG)</span></span></li>
+                        <li><span><span>
+                            <svg width="16" height="13" viewBox="0 0 16 13" fill="#f63" xmlns="http://www.w3.org/2000/svg" class="mr2 orange0">
+                                <path d="M15.472 2.036L13.436 0L5.497 7.94L2.036 4.479L0 6.514L5.497 12.011L15.472 2.036Z" fill="#f63"></path>
+                            </svg>
+                        </span><span>Browser Isolation & Email Security</span></span></li>
+                        <li><span><span>
+                            <svg width="16" height="13" viewBox="0 0 16 13" fill="#f63" xmlns="http://www.w3.org/2000/svg" class="mr2 orange0">
+                                <path d="M15.472 2.036L13.436 0L5.497 7.94L2.036 4.479L0 6.514L5.497 12.011L15.472 2.036Z" fill="#f63"></path>
+                            </svg>
+                        </span><span>Cloud Access Security Broker (CASB)</span></span></li>
+                    </ul>
+                </div>
+                <div>
+                    <a href="#5"><span><span>View Plans & Pricing</span><span><i class='bx bx-chevron-right' ></i></span></span></a>
+                </div>
+                <hr>
+                <div><a href="#5">Learn More</a></div>
+            </div>
+             <div>
+                <div></div>
+                <div>
+                     <h3></h3>
+                    <p></p>
+                </div>
+                <div>
+                    <ul>
+                        <li><span><span>
+                            <svg width="16" height="13" viewBox="0 0 16 13" fill="#f63" xmlns="http://www.w3.org/2000/svg" class="mr2 orange0">
+                                <path d="M15.472 2.036L13.436 0L5.497 7.94L2.036 4.479L0 6.514L5.497 12.011L15.472 2.036Z" fill="#f63"></path>
+                            </svg>
+                        </span><span></span></span></li>
+                        <li><span><span>
+                            <svg width="16" height="13" viewBox="0 0 16 13" fill="#f63" xmlns="http://www.w3.org/2000/svg" class="mr2 orange0">
+                                <path d="M15.472 2.036L13.436 0L5.497 7.94L2.036 4.479L0 6.514L5.497 12.011L15.472 2.036Z" fill="#f63"></path>
+                            </svg>
+                        </span><span></span></span></li>
+                        <li><span><span>
+                            <svg width="16" height="13" viewBox="0 0 16 13" fill="#f63" xmlns="http://www.w3.org/2000/svg" class="mr2 orange0">
+                                <path d="M15.472 2.036L13.436 0L5.497 7.94L2.036 4.479L0 6.514L5.497 12.011L15.472 2.036Z" fill="#f63"></path>
+                            </svg>
+                        </span><span></span></span></li>
+                        <li><span><span>
+                            <svg width="16" height="13" viewBox="0 0 16 13" fill="#f63" xmlns="http://www.w3.org/2000/svg" class="mr2 orange0">
+                                <path d="M15.472 2.036L13.436 0L5.497 7.94L2.036 4.479L0 6.514L5.497 12.011L15.472 2.036Z" fill="#f63"></path>
+                            </svg>
+                        </span><span></span></span></li>
+                        <li><span><span>
+                            <svg width="16" height="13" viewBox="0 0 16 13" fill="#f63" xmlns="http://www.w3.org/2000/svg" class="mr2 orange0">
+                                <path d="M15.472 2.036L13.436 0L5.497 7.94L2.036 4.479L0 6.514L5.497 12.011L15.472 2.036Z" fill="#f63"></path>
+                            </svg>
+                        </span><span></span></span></li>
+                    </ul>
+                </div>
+                <div>
+                    <a href="#5"><span><span></span><span></span></span></a>
+                </div>
+                <hr>
+                <div><a href="#5">Learn More</a></div>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<style>
+    .benefits li {
+        margin-bottom: 1rem;
+    }
+    .benefits li::before {
+        content: "•";
+        margin-right: 1.3rem;
+        color: #f6821f
+    }
+</style>
